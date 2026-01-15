0ddc98b87ebb2b9f40d6b4fde3ede5833d917d08
pijush modal <pijushmondal617@gmail.com>
2026-01-16 00:31:32 +0530

    new files added

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Villa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+    }
+}
